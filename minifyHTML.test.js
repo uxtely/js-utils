0a43e3e68388bc4f951922d391622e7ea7e4a82f
Eric Fortis <efortis@gmail.com>
2022-04-22 06:59:48 -0400

    add simple minifiers for CSS and HTML

---
+++ minifyHTML.test.js
@@ -0,0 +1,67 @@
+import { strictEqual } from 'assert'
+import { minifyHTML } from './minifyHTML.js';
+
+
+strictEqual(minifyHTML(`
+<!DOCTYPE html>
+<html   
+  lang="en">
+  <!-- Comment -->
+<head>
+  <!-- Mulitline 
+            Comment -->
+	<!--		&lt;!&ndash;	deleteme '  '	.&ndash;&gt;-->
+	<title>Foo Bar</title>
+</head>
+<body>
+	<div>
+		<h1>Title</h1>
+  </div>
+  <p>Single Quotes? Keep'em a'll</p>
+  <pre data-custom-attr="keeps   everything here <h1>   <h1>">
+   aaa   bbbb
+   ccc   dddd
+  
+  </pre>
+  <dd>
+  <kbd>A</kbd>
+  <kbd class="foo">B</kbd>
+  <kbd>Ctrl</kbd>Click
+  </dd>
+  <a>LinkA</a>
+  <a>LinkB</a>
+  <p>
+		Removes leading tabs.
+  </p>
+</body>
+</html>
+`),
+
+	`<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Foo Bar</title>
+</head>
+<body>
+<div>
+<h1>Title</h1>
+</div>
+<p>Single Quotes? Keep'em a'll</p>
+<pre data-custom-attr="keeps   everything here <h1>   <h1>">
+   aaa   bbbb
+   ccc   dddd
+  
+  </pre>
+<dd>
+<kbd>A</kbd>
+<kbd class="foo">B</kbd>
+<kbd>Ctrl</kbd>Click
+</dd>
+<a>LinkA</a>
+<a>LinkB</a>
+<p>
+Removes leading tabs.
+</p>
+</body>
+</html>`
+)
