cfe32e1dfad688e4bfe18ad4822d1a61e233f256
Eric Fortis <efortis@gmail.com>
2022-05-30 15:47:29 -0400

    format

--- static-pages-bundler/parsers.js
+++ static-pages-bundler/parsers.js
@@ -1,23 +1,10 @@
 import { removeCommentsFromHTML } from './minifyHTML.js'
 
 
-/**
- * NOTE: Run minifyHTML first, because using any of these functions
- * because they don't support tags within comments nor multiline ones.
- */
+export const extractStyleSheetHrefs = html => Array.from(html.matchAll(/(?<=<link\s.*href=")[^."]+\.css/g), m => m[0])
+export const extractJavaScriptSources = html => Array.from(html.matchAll(/(?<=<script\s.*src=")[^."]+\.js/g), m => m[0])
 
-
-const reExtractStyleSheets = /(?<=<link\s.*href=")[^."]+\.css/g
-const reExtractScripts = /(?<=<script\s.*src=")[^."]+\.js/g
-
-export const extractStyleSheetHrefs = html =>
-	Array.from(html.matchAll(reExtractStyleSheets), m => m[0])
-
-export const extractJavaScriptSources = html =>
-	Array.from(html.matchAll(reExtractScripts), m => m[0])
-
-export const removeLineContaining = (html, str) =>
-	html.replace(new RegExp('^.*' + str + '.*\n', 'm'), '')
+export const removeLineContaining = (html, str) => html.replace(new RegExp('^.*' + str + '.*\n', 'm'), '')
 
 
 /**
