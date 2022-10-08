48112889ad49a7642f9548880d85aabb29801138
Eric Fortis <efortis@gmail.com>
2022-10-08 12:15:17 -0400

    refactor

--- local-storage.js
+++ local-storage.js
@@ -1,35 +1,34 @@
-/** 
- * By spec, getting a non-existing item returns null
- *   https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem
- */
+// window.localStorage:
+//   - Non-existing items return `null`
+//   - `localStorage` throws when cookies are disabled
+//   - https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem
 
-export function readLocalStorageBoolean(key, failover) {
+export function readLocalStorageBoolean(key, defaultValue) {
 	try {
 		const item = localStorage.getItem(key)
 		return item === null
-			? failover
-			: !!+item
+			? defaultValue
+			: Boolean(Number(item))
 	}
 	catch (_) {
-		return failover
+		return defaultValue
 	}
 }
 
-
-export function readLocalStorageNumber(key, failover) {
+export function readLocalStorageNumber(key, defaultValue) {
 	try {
 		const item = localStorage.getItem(key)
 		return item === null
-			? failover
-			: +item
+			? defaultValue
+			: Number(item)
 	}
 	catch (_) {
-		return failover
+		return defaultValue
 	}
 }
-
 
 export function setLocalStorageItem(key, value) {
-	try { localStorage.setItem(key, +value) } // Ensures it is numeric
+	try { localStorage.setItem(key, Number(value)) }
 	catch {}
 }
+
