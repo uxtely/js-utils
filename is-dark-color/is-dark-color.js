f89713624a277a042f137bd99e1557eb3d6bad02
Eric Fortis <efortis@gmail.com>
2022-12-29 16:34:27 -0400

    ref

--- is-dark-color/is-dark-color.js
+++ is-dark-color/is-dark-color.js
@@ -1,11 +1,10 @@
 const MID_GREY = 137
-const firstHashRegex = /^#/
 const reValidColor = /^#[\da-f]{6}$/i
 
 export function isDarkColor(color) {
 	if (!reValidColor.test(color))
 		throw `Invalid color format: "${color}". It must have a "#" and 6 hex digits`
-	const c = parseInt(color.replace(firstHashRegex, ''), 16)
+	const c = parseInt(color.substring(1), 16)
 	const r = 0.299 * ((c & 0xff0000) >> 16)
 	const g = 0.587 * ((c & 0xff00) >> 8)
 	const b = 0.114 * (c & 0xff)
